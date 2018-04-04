8b082f9c806190f24ce41300f73ceb63cb9d6223
Tobias Schneider <tobias.schneider@tobsch.org>
2018-04-04 22:25:36 +0200

    Open RSS link in new window

--- src/main/java/coffee/synyx/frontpage/plugin/rss/HtmlConverter.java
+++ src/main/java/coffee/synyx/frontpage/plugin/rss/HtmlConverter.java
@@ -12,15 +12,15 @@ class HtmlConverter {
         String html = "";
         for (BlogEntry entry : entries) {
             html += "<div style=\"margin-bottom: 25px;\">";
-            html += "<header>";
-            html += "<h3 style=\"margin-bottom: 0;\">";
-            html += "<a style=\"color: black; text-decoration: none; font-size: large;\" href=\"" + entry.getLink() + "\">" + entry.getTitle() + "</a>";
-            html += "</h3>";
-            html += "<address style=\"font-size: smaller;\">" + entry.getAuthor() + " - " + entry.getPublishDate() + "</address>";
-            html += "</header>";
-            html += "<div style=\"margin-top: 10px;\">";
-            html += "<a style=\"text-decoration: none; color: inherit; font-size: small;\" href=\"" + entry.getLink() + "\"><span>" + entry.getDescription() + "</span></a>";
-            html += "</div>";
+            html += "  <header>";
+            html += "    <h3 style=\"margin-bottom: 0;\">";
+            html += "      <a style=\"color: black; text-decoration: none; font-size: large;\" target=\"_blank\" rel=\"noopener\" href=\"" + entry.getLink() + "\">" + entry.getTitle() + "</a>";
+            html += "    </h3>";
+            html += "    <address style=\"font-size: smaller;\">" + entry.getAuthor() + " - " + entry.getPublishDate() + "</address>";
+            html += "  </header>";
+            html += "  <div style=\"margin-top: 10px;\">";
+            html += "    <a style=\"text-decoration: none; color: inherit; font-size: small;\" target=\"_blank\" rel=\"noopener\" href=\"" + entry.getLink() + "\"><span>" + entry.getDescription() + "</span></a>";
+            html += "  </div>";
             html += "</div>";
         }
         return html;
