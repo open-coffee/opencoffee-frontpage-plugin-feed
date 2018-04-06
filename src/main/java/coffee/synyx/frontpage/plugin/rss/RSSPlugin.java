7c4f441f224258d6495eee1aef9530acfb3af342
Benjamin Seber <seber@synyx.de>
2018-04-06 14:57:50 +0200

    integrated first configuration options

--- src/main/java/coffee/synyx/frontpage/plugin/rss/RSSPlugin.java
+++ src/main/java/coffee/synyx/frontpage/plugin/rss/RSSPlugin.java
@@ -1,22 +1,41 @@
 package coffee.synyx.frontpage.plugin.rss;
 
-import coffee.synyx.frontpage.plugin.api.FrontpagePluginInterface;
+import coffee.synyx.frontpage.plugin.api.ConfigurationDescription;
+import coffee.synyx.frontpage.plugin.api.ConfigurationField;
+import coffee.synyx.frontpage.plugin.api.ConfigurationInstance;
+import coffee.synyx.frontpage.plugin.api.FrontpagePlugin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
 import static coffee.synyx.frontpage.plugin.rss.HtmlConverter.toHtml;
 import static java.lang.invoke.MethodHandles.lookup;
+import static java.util.Arrays.asList;
 
 @Component
-public class RSSPlugin implements FrontpagePluginInterface {
+public class RSSPlugin implements FrontpagePlugin {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(lookup().lookupClass());
 
-    private static final String BLOG_URL = "https://www.synyx.de/blog/index.xml";
-    private static final int BLOG_ENTRY_COUNT = 10;
-    private static final int DESCRIPTION_LENGTH = 200;
+    private static final String RSS_FIELD_TITLE = "rss.field.title";
+    private static final String RSS_FIELD_URL = "rss.field.url";
+    private static final String RSS_FIELD_ENTRY_COUNT = "rss.field.entry.count";
+    private static final String RSS_FIELD_ENTRY_LENGTH = "rss.field.entry.length";
+
+    private static final Set<ConfigurationField> CONFIGURATION_FIELDS = Collections.unmodifiableSet(new HashSet<>(
+        asList(
+            createField("Title", "text", RSS_FIELD_TITLE),
+            createField("URL", "text", RSS_FIELD_URL),
+            createField("Anzahl Artikel", "text", RSS_FIELD_ENTRY_COUNT),
+            createField("Teaser Text Länge", "text", RSS_FIELD_ENTRY_LENGTH)
+        )
+    ));
 
     private final BlogParser blogParser;
 
@@ -25,18 +44,42 @@ public class RSSPlugin implements FrontpagePluginInterface {
         this.blogParser = blogParser;
     }
 
-    @Override
-    public String title() {
-        return "Synyx Blog";
+    private static ConfigurationField createField(final String label, final String type, final String id) {
+        return new ConfigurationField() {
+            @Override
+            public String getLabel() {
+                return label;
+            }
+
+            @Override
+            public String getType() {
+                return type;
+            }
+
+            @Override
+            public String getId() {
+                return id;
+            }
+        };
     }
 
     @Override
-    public String content() {
+    public String title(ConfigurationInstance configurationInstance) {
+        return configurationInstance.get(RSS_FIELD_TITLE);
+    }
+
+    @Override
+    public String content(ConfigurationInstance configurationInstance) {
+
+        final String feedUrl = configurationInstance.get(RSS_FIELD_URL);
+        final int entryCount = Integer.parseInt(configurationInstance.get(RSS_FIELD_ENTRY_COUNT));
+        final int entryLength = Integer.parseInt(configurationInstance.get(RSS_FIELD_ENTRY_LENGTH));
+
         String content = "";
         try {
-            content = toHtml(blogParser.parse(BLOG_URL, BLOG_ENTRY_COUNT, DESCRIPTION_LENGTH));
+            content = toHtml(blogParser.parse(feedUrl, entryCount, entryLength));
         } catch (ParserException e) {
-            LOGGER.error("RSS Plugin: Could not receive rss feed from {}", BLOG_URL);
+            LOGGER.error("RSS Plugin: Could not receive rss feed from {}", feedUrl);
         }
 
         return content;
@@ -45,5 +88,10 @@ public class RSSPlugin implements FrontpagePluginInterface {
     @Override
     public String id() {
         return "rss";
+    }
+
+    @Override
+    public Optional<ConfigurationDescription> getConfigurationDescription() {
+        return Optional.of(() -> CONFIGURATION_FIELDS);
     }
 }
