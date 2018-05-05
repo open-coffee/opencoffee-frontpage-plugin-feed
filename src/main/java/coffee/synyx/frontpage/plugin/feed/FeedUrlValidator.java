8f0e537d696f185dd0bcd088fb5d107dc14cac26
Benjamin Seber <seber@synyx.de>
2018-05-05 11:50:23 +0200

    added confgiurationFieldValidator for url field

---
+++ src/main/java/coffee/synyx/frontpage/plugin/feed/FeedUrlValidator.java
@@ -0,0 +1,33 @@
+package coffee.synyx.frontpage.plugin.feed;
+
+import coffee.synyx.frontpage.plugin.api.ConfigurationField;
+import coffee.synyx.frontpage.plugin.api.validation.ConfigurationFieldValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+
+@Component
+public class FeedUrlValidator implements ConfigurationFieldValidator {
+
+    private final FeedParser feedParser;
+
+    @Autowired
+    public FeedUrlValidator(FeedParser feedParser) {
+        this.feedParser = feedParser;
+    }
+
+    @Override
+    public boolean supports(ConfigurationField field) {
+        // TODO is the id enough? there could be collisions with other plugins since it is a string
+        return FeedPlugin.FEED_FIELD_URL.equals(field.getId());
+    }
+
+    @Override
+    public void validate(Object target, ConfigurationField field, Errors errors) {
+        try {
+            feedParser.parse(String.valueOf(target), 1, 42);
+        } catch(ParserException e) {
+            errors.rejectValue(field.getId(), "plugin.feed.validation.error.url");
+        }
+    }
+}
